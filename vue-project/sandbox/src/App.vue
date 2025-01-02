550f7052864497cb5c4a89a2600b11cf0569e64b
Kamil Aliyev <kamilraliyev@gmail.com>
2025-01-02 02:00:57 -0500

    Seperating components

--- vue-project/sandbox/src/App.vue
+++ vue-project/sandbox/src/App.vue
@@ -1,68 +1,34 @@
+<!-- eslint-disable vue/block-lang -->
 <script>
-export default {
-  data() {
-        return {
-          message: "Hello, it works!",
-          listOfNumbers: [1,2,3,4,5],
-          count: 10,
-          incrementAmount: 19,
-          counterTitle: "Cool number"
-        }
-      },
-      computed: {
-        // Compute date is performance optimized calculations on reactive data
-        // Vue nows when to run it.
-        displayTitle() {
-          if (this.count > 100){
-            return "Cool number - very long."
-          }
-          else{
-            return "Cool number"
-          }
-        },
-        optimizedIncrementAmount(){
-          return this.displayTitle.length * this.incrementAmount
-        }
+import CounterBase from './components/CounterBase.vue';
 
-      },
-      methods: {
-        incrementCount(){
-          this.count+=this.optimizedIncrementAmount
-        },
-        changeIncrementAmount(event){
-          this.incrementAmount = event.target.value
-        }
-      },
-      watch: {
-        // it is good for when you need a side effect on your codebase.
-        // Let's say when you need to make specific api call on specific state.
-        // watch for triggering actions, computed data for computing reactive data
-        count(newValue){
-            if (newValue > 1000){
-              this.counterTitle = "oof, your number is big"
-            }
-        }
+export default {
+  components: {
+    CounterBase,
+  },
+  data() {
+    return {
+      message: "Hello, it works!",
+      listOfNumbers: [1, 2, 3, 4, 5],
     }
+  },
+  computed: {
+    // Compute date is performance optimized calculations on reactive data
+    // Vue nows when to run it.
+
+  },
+  methods: {
+
+  },
 }
 </script>
 
 <template>
-  <h2>{{displayTitle}}</h2>
-      <p v-if="message.length%2 ==0">Even: {{message}}</p>
-      <p v-else>Odd: {{message}}</p>
-      <ul>
-        <li v-for="number in listOfNumbers">{{number}}</li>
-      </ul>
-      <hr>
-      <p>Count: {{count}}</p>
-      <p>Increment Amount: {{incrementAmount}}</p>
-      <p>Optimized Increment Amount: {{optimizedIncrementAmount}}</p>
-      <button v-on:click="incrementCount">
-        Increment Count
-      </button>
-      <hr>
-      <div>
-        <label for="incrementAmount">Increment by:</label>
-        <input type="number" id="incrementAmount" v-model="incrementAmount">
-      </div>
+  <p v-if="message.length % 2 == 0">Even: {{ message }}</p>
+  <p v-else>Odd: {{ message }}</p>
+  <ul>
+    <li v-for="(number, id) in listOfNumbers" :key="`number-${id}`">{{ number }}</li>
+  </ul>
+  <hr>
+  <counter-base />
 </template>
---
+++ vue-project/sandbox/src/components/CounterBase.vue
@@ -0,0 +1,50 @@
+<!-- eslint-disable vue/block-lang -->
+<script>
+export default {
+  data() {
+    return {
+      count: 10,
+      incrementAmount: 19,
+      counterTitle: "Cool number",
+    }
+  },
+  computed: {
+    displayTitle() {
+      if (this.count > 100) {
+        return "Cool number - very long."
+      }
+      else {
+        return "Cool number"
+      }
+    },
+    optimizedIncrementAmount() {
+      return this.displayTitle.length * this.incrementAmount
+    }
+
+  },
+  methods: {
+    incrementCount() {
+      this.count += this.optimizedIncrementAmount
+    },
+    changeIncrementAmount(event) {
+      this.incrementAmount = event.target.value
+    }
+  }
+}
+</script>
+
+<template>
+  <h2>{{ displayTitle }}</h2>
+  <hr>
+  <p>Count: {{ count }}</p>
+  <p>Increment Amount: {{ incrementAmount }}</p>
+  <p>Optimized Increment Amount: {{ optimizedIncrementAmount }}</p>
+  <button v-on:click="incrementCount">
+    Increment Count
+  </button>
+  <hr>
+  <div>
+    <label for="incrementAmount">Increment by:</label>
+    <input type="number" id="incrementAmount" v-model="incrementAmount">
+  </div>
+</template>
