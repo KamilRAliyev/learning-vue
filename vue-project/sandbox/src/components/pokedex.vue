0048d469f47913f13f7a3c3ae6b0be84f8e6e172
Kamil Aliyev <kamilraliyev@gmail.com>
2025-01-03 16:57:46 -0500

    Composition API: setup

--- vue-project/sandbox/src/components/pokedex.vue
+++ vue-project/sandbox/src/components/pokedex.vue
@@ -1,46 +1,44 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <!-- eslint-disable vue/block-lang -->
-<script>
-import { computed, ref, reactive } from 'vue';
+<script setup>
+import { computed, ref, reactive, defineProps, defineEmits } from 'vue';
+import BaseButton from './BaseButton.vue';
 // Reactive is, think of data{} option from options api, packed in a nice helper function
 // What if I want to call data state{}?
+const regionName = ref('Keln');
+const emits = defineEmits(['change-region'])
+const pokedox = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then((response) => response.json());
 
-export default {
-  async setup() {
-    const regionName = ref('Keln');
-    const pokedox = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then((response) => response.json());
+const state = reactive({
+  elementType: 'lightning',
+});
 
-    const state = reactive({
-      elementType: 'lightning',
-    })
+const regionNameAllCaps = computed(() => { return state.elementType.toUpperCase() });
 
-    const regionNameAllCaps = computed(() => { return state.elementType.toUpperCase() })
-    console.log(state)
-    return {
-      pokedox, regionName, regionNameAllCaps
-    }
-  },
-  computed: {
-    regionNameLowerCase() {
-      return this.regionName.toLowerCase()
+const changeRegionName = () => {
+  regionName.value = "Hoenn"
+  emits('change-region');
+};
+
+const props = defineProps(
+  {
+    region: {
+      type: String,
+      default: "Kanto",
     },
-  },
-  methods: {
-    changeRegionName() {
-      this.regionName = "Hoenn"
-    }
-  },
-  created() {
-    console.log(this.pokedox)
-  },
-}
+  }
+);
+
+const elementTypeAllCaps = computed(() => { return state.elementType.toUpperCase() + props.region })
+
 </script>
 
 <template>
+  <h3>{{ elementTypeAllCaps }}</h3>
   <h1>{{ regionName }}</h1>
   <h3>{{ regionNameAllCaps }}</h3>
-  <h3>{{ regionNameLowerCase }}</h3>
   <button @click="changeRegionName">Change Region Name</button>
   <h1>New App</h1>
   <pre>{{ pokedox }}</pre>
+  <base-button left="true"></base-button>
 </template>
