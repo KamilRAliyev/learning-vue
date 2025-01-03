bb93c8f7a40d51f796cc758c8b5dcaa43690f0b6
Kamil Aliyev <kamilraliyev@gmail.com>
2025-01-02 23:08:57 -0500

    Snapshot: Beginning

--- vue-project/sandbox/src/App.vue
+++ vue-project/sandbox/src/App.vue
@@ -1,6 +1,26 @@
 <script>
+// https://pokeapi.co/api/v2/pokemon?limit=151
+export default {
+  created() {
+    this.fetchPokemon()
+  },
+  data: () => {
+    return {
+      pokedox: {},
+    }
+  },
+  methods: {
+    async fetchPokemon() {
+      this.pokedox = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then((response) => response.json());
+    },
+  },
+
+}
 </script>
 
 <template>
-
+  <h1>New App</h1>
+  <pre>{{ pokedox }}</pre>
 </template>
+
+<style></style>
