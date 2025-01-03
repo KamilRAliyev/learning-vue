8484fb6ced17c32b933fc228b701b99fe57752c7
Kamil Aliyev <kamilraliyev@gmail.com>
2025-01-03 13:13:22 -0500

    Composition API: computed property 2

--- vue-project/sandbox/src/components/pokedex.vue
+++ vue-project/sandbox/src/components/pokedex.vue
@@ -5,7 +5,7 @@ import { computed, ref } from 'vue';
 
 export default {
   async setup() {
-    const regionName = ref('keln');
+    const regionName = ref('Keln');
     const regionNameAllCaps = computed(() => { return regionName.value.toUpperCase() })
     const pokedox = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then((response) => response.json());
 
@@ -13,6 +13,11 @@ export default {
     return {
       pokedox, regionName, regionNameAllCaps
     }
+  },
+  computed: {
+    regionNameLowerCase() {
+      return this.regionName.toLowerCase()
+    },
   },
   methods: {
     changeRegionName() {
@@ -28,6 +33,7 @@ export default {
 <template>
   <h1>{{ regionName }}</h1>
   <h3>{{ regionNameAllCaps }}</h3>
+  <h3>{{ regionNameLowerCase }}</h3>
   <button @click="changeRegionName">Change Region Name</button>
   <h1>New App</h1>
   <pre>{{ pokedox }}</pre>
