f448f5c33295554d2445e4d5a66c67dbc12bcf3a
Kamil Aliyev <kamilraliyev@gmail.com>
2025-01-02 23:44:42 -0500

    Exercise: Lifecycles

---
+++ vue-project/cest-la-vue/src/components/UserPage.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  data: () => {
+    return {
+      users: {},
+    };
+  },
+  created() {
+    this.fetchUsers();
+  },
+  methods: {
+    async fetchUsers() {
+      this.users = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      ).then((response) => response.json());
+    },
+  },
+};
+</script>
+
+<template>
+  <ul>
+    <li v-for="user in users" :key="`user-${user.id}`">
+      {{ user.name }}: {{ user.website }}
+    </li>
+  </ul>
+</template>
