97e8bf37f4174f39f205b235275e6a2b8ac3036d
Kamil Aliyev <kamilraliyev@gmail.com>
2025-01-02 22:06:05 -0500

    Exercise: Slot - building layout

--- vue-project/show-project/src/App.vue
+++ vue-project/show-project/src/App.vue
@@ -1,12 +1,14 @@
 <!-- eslint-disable vue/block-lang -->
 <script>
+import BaseLayout from "./components/BaseLayout.vue";
 import BenderStatistics from "./components/BenderStatistics.vue"
 import CharacterCard from "./components/CharacterCard.vue";
 
 export default {
   components: {
     BenderStatistics,
-    CharacterCard
+    CharacterCard,
+    BaseLayout,
   },
   data: () => (
     {
@@ -54,24 +56,32 @@ export default {
 </script>
 
 <template>
-  <p v-if="characterList.length === 0">There is no Charachter</p>
-  <ul v-else>
-    <li v-for="(char, id) in characterList" :key="`bender-${id}`">
-      <CharacterCard :character="char" @favorite="addToFavorite" />
-    </li>
-  </ul>
-  <hr>
-  <p>Hey Your favorite Charachters</p>
-  <ul>
-    <li v-for="(char, id) in favoriteCharacters" :key="`character-${id}`">{{ char.name }}</li>
-  </ul>
-  <hr>
-  <p>New Char adding form</p>
-  <pre>{{ newCharachter }}</pre>
+  <base-layout>
+    <template #one>
+      <hr>
+      <p>New Char adding form</p>
+      <pre>{{ newCharachter }}</pre>
 
-  <label for="newCharName">New Charachter Name </label>
-  <input type="text" id="newCharName" v-model="newCharachter.name" @keyup.enter="addNewCharachter">
-  <hr>
-  <BenderStatistics :characters="characterList" />
+      <label for="newCharName">New Charachter Name </label>
+      <input type="text" id="newCharName" v-model="newCharachter.name" @keyup.enter="addNewCharachter">
+      <hr>
+      <BenderStatistics :characters="characterList" />
+    </template>
+    <template #two>
+      <p v-if="characterList.length === 0">There is no Charachter</p>
+      <ul v-else>
+        <li v-for="(char, id) in characterList" :key="`bender-${id}`">
+          <CharacterCard :character="char" @favorite="addToFavorite" />
+        </li>
+      </ul>
+      <hr>
+      <p>Hey Your favorite Charachters</p>
+      <ul>
+        <li v-for="(char, id) in favoriteCharacters" :key="`character-${id}`">{{ char.name }}</li>
+      </ul>
+    </template>
+  </base-layout>
+
+
 
 </template>
---
+++ vue-project/show-project/src/components/BaseLayout.vue
@@ -0,0 +1,24 @@
+<!-- eslint-disable vue/block-lang -->
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <div class="grid">
+    <div>
+      <slot name="one"></slot>
+    </div>
+    <div>
+      <slot name="two"></slot>
+    </div>
+  </div>
+</template>
+
+<style>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+}
+</style>
