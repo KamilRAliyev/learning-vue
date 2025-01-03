f879ee2150ef9de382b915895913f495e858b90c
Kamil Aliyev <kamilraliyev@gmail.com>
2025-01-03 02:26:26 -0500

    Composition API: Reactivity

--- vue-project/sandbox/src/components/pokedex.vue
+++ vue-project/sandbox/src/components/pokedex.vue
@@ -1,12 +1,20 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <!-- eslint-disable vue/block-lang -->
 <script>
+import { ref } from 'vue';
+
 export default {
   async setup() {
+    const regionName = ref('keln');
     const pokedox = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then((response) => response.json());
 
     return {
-      pokedox,
+      pokedox, regionName
+    }
+  },
+  methods: {
+    changeRegionName() {
+      this.regionName = "Hoenn"
     }
   },
   created() {
@@ -16,6 +24,8 @@ export default {
 </script>
 
 <template>
+  <h1>{{ regionName }}</h1>
+  <button @click="changeRegionName">Change Region Name</button>
   <h1>New App</h1>
   <pre>{{ pokedox }}</pre>
 </template>
