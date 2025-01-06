ab67eef622bec2f5bce416baf9f962ff3efd5482
Kamil Aliyev <kamilraliyev@gmail.com>
2025-01-05 22:18:39 -0500

    CSS: v-bind vars

--- vue-project/sandbox/src/App.vue
+++ vue-project/sandbox/src/App.vue
@@ -1,8 +1,10 @@
 <!-- eslint-disable vue/block-lang -->
 <script setup>
-import { reactive } from 'vue';
+import { ref, reactive } from 'vue';
 import CounterBase from './components/CounterBase.vue';
 import UserCard from './components/userCard.vue';
+
+const colorPreferred = ref("black");
 
 const userr = reactive({
   name: "Aliye",
@@ -11,16 +13,24 @@ const userr = reactive({
 </script>
 
 <template>
-  <CounterBase></CounterBase>
-  <h2>
-    <userCard :user="userr" />
-  </h2>
+  <div class="wrapper">
+    <h2>color: {{ colorPreferred }}</h2>
+    <input type="color" v-model="colorPreferred">
+    <CounterBase></CounterBase>
+    <h2>
+      <userCard :user="userr" />
+    </h2>
+  </div>
 </template>
 
 <style>
 /* The Problem with vue is that it just injects it  globally for classic style, that's why we need style scoped */
 html {
   background-color: papayawhip;
+}
+
+.wrapper {
+  background-color: v-bind(colorPreferred);
 }
 
 button {
